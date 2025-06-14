e834b938dcf364ec4a65cd719c4ec521d4871b6c
NicaZadirieva <no.demchenko@yandex.ru>
2025-06-14 21:22:31 +0300

    chore: настроила .eslint

--- top-app-old/eslint.config.mjs
+++ top-app-old/eslint.config.mjs
@@ -1,16 +1,29 @@
-import { dirname } from "path";
-import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
+import { FlatCompat } from '@eslint/eslintrc';
+import { dirname } from 'path';
+import { fileURLToPath } from 'url';
+
+
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
 const compat = new FlatCompat({
-  baseDirectory: __dirname,
+	baseDirectory: __dirname
 });
 
 const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
+	...compat.extends('next/core-web-vitals', 'next/typescript'),
+	{
+		rules: {
+			'semi': ['error', 'always',
+				{
+					'omitLastInOneLineBlock': false
+				}],
+			'comma-dangle': ['error', 'never'],
+			'quotes': ['error', 'single'],
+			'indent': ['error', 'tab']
+		}
+	}
 ];
 
 export default eslintConfig;
