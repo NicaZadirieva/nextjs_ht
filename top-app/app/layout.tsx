78685424067edb7bb1c470723608d0a5ca0195d9
NicaZadirieva <no.demchenko@yandex.ru>
2025-06-25 10:59:07 +0300

    удалила изображения и добавила шрифт к новому next

--- top-app/app/layout.tsx
+++ top-app/app/layout.tsx
@@ -1,16 +1,7 @@
+/* eslint-disable @next/next/no-page-custom-font */
 import type { Metadata } from 'next';
-import { Geist, Geist_Mono } from 'next/font/google';
 import './globals.css';
 
-const geistSans = Geist({
-	variable: '--font-geist-sans',
-	subsets: ['latin']
-});
-
-const geistMono = Geist_Mono({
-	variable: '--font-geist-mono',
-	subsets: ['latin']
-});
 
 export const metadata: Metadata = {
 	title: 'Наш проект',
@@ -24,8 +15,15 @@ export default function RootLayout({
 }>) {
 	return (
 		<html lang="en">
-			<body className={`${geistSans.variable} ${geistMono.variable}`}>
+			<head>
+				<link rel="preconnect" href="https://fonts.googleapis.com" />
+				<link rel="preconnect" href="https://fonts.gstatic.com" />
+				<link href="https://fonts.googleapis.com/css2?family=Noto+Sans:wght@100..900&display=swap" rel="stylesheet" />
+			</head>
+			
+			<body>
 				{children}
+
 			</body>
 		</html>
 	);
