54b90408012c1d2fc2af1c14cfeaffd1cea434b7
NicaZadirieva <no.demchenko@yandex.ru>
2025-06-27 19:48:01 +0300

    demo: show BlogItem

--- blog/app/page.tsx
+++ blog/app/page.tsx
@@ -1,7 +1,16 @@
+import { BlogItem } from '@/entities';
+
 export default function Home() {
 	return (
 		<div>
-      Home
+			<BlogItem 
+				text="The CSS Grid Layout Module offers a grid-based layout system, with rows and columns, making it easier to design web pages without having to use floats and positioning." 
+				createdAt={new Date()}
+				tag='Frontend'
+				title="Css Grid"
+				readTime="3 Min Read"
+				postLink="google.com"
+				mainPhotoLink='/Blog/blogPost.jpg'/>
 		</div>
 	);
 }
