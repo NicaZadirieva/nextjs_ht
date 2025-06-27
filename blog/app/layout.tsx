52e5eacc6efa1158ac491e276f253bcf090d9fe6
NicaZadirieva <no.demchenko@yandex.ru>
2025-06-27 20:04:55 +0300

    feat: добавила шрифты

--- blog/app/layout.tsx
+++ blog/app/layout.tsx
@@ -1,4 +1,24 @@
+import localFont from 'next/font/local';
 import './globals.css';
+
+const SFText = localFont({
+	src: [
+		{
+			path: './font/SFProText-Regular.ttf',
+			weight: '400',
+			style: 'normal'
+		},
+		{
+			path: './font/SFProText-Medium.ttf',
+			weight: '500',
+			style: 'normal'
+		},
+		{
+			path: './font/SFProText-Bold.ttf',
+			weight: '700',
+			style: 'normal'
+		}]
+});
 
 export default function RootLayout({
 	children
@@ -6,7 +26,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
 	return (
-		<html lang="en">
+		<html lang="en" className={SFText.className}>
 			<body>
 				{children}
 			</body>
