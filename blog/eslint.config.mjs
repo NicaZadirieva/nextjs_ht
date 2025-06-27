8ccf5a5b96dcac38a6c2e41d133a4335f3e3b345
NicaZadirieva <no.demchenko@yandex.ru>
2025-06-27 19:45:34 +0300

    chore: add config for stylelint

--- blog/eslint.config.mjs
+++ blog/eslint.config.mjs
@@ -8,28 +8,28 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
 const compat = new FlatCompat({
-  baseDirectory: __dirname
+	baseDirectory: __dirname
 });
 
 const eslintConfig = [
-  ...compat.extends('next/core-web-vitals', 'next/typescript'),
-  {
-    rules: {
-      'semi': ['error', 'always',
-        {
-          'omitLastInOneLineBlock': false
-        }],
-      'comma-dangle': ['error', 'never'],
-      'quotes': ['error', 'single'],
-      'indent': ['error', 'tab'],
-      //Ошибка, если используется конкатенация строк вместо шаблонных литералов.
-      'prefer-template': ['error'],
-      //Ошибка, если в качестве коллбека используется не стрелочная функция.
-      'prefer-arrow-callback': ['error'],
-      //Ошибка, если в одном из ветвлений функция возвращает значение, а в другом нет.
-      'consistent-return': 'error'
-    }
-  }
+	...compat.extends('next/core-web-vitals', 'next/typescript'),
+	{
+		rules: {
+			'semi': ['error', 'always',
+				{
+					'omitLastInOneLineBlock': false
+				}],
+			'comma-dangle': ['error', 'never'],
+			'quotes': ['error', 'single'],
+			'indent': ['error', 'tab'],
+			//Ошибка, если используется конкатенация строк вместо шаблонных литералов.
+			'prefer-template': ['error'],
+			//Ошибка, если в качестве коллбека используется не стрелочная функция.
+			'prefer-arrow-callback': ['error'],
+			//Ошибка, если в одном из ветвлений функция возвращает значение, а в другом нет.
+			'consistent-return': 'error'
+		}
+	}
 ];
 
 export default eslintConfig;
