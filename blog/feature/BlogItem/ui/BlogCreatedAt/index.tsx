9c35d19845d102169fd3bcf9ca6f77f11bd7e4a8
NicaZadirieva <no.demchenko@yandex.ru>
2025-06-30 08:04:25 +0300

    refactor: renaming of daysBetweenNow

--- blog/feature/BlogItem/ui/BlogCreatedAt/index.tsx
+++ blog/feature/BlogItem/ui/BlogCreatedAt/index.tsx
@@ -4,12 +4,13 @@ import styles from './index.module.css';
 export const BlogCreatedAt = ({ createdAt }: BlogCreatedAtProps) => {
 	return (
 		<span className={styles['created-at']}>
-			{daysBetweenNow(createdAt)} days
+			{daysUpToNow(createdAt)} days
 		</span>
 	);
 };
 
-function daysBetweenNow(startDate: Date) {
+// оставлю как заглушку
+function daysUpToNow(startDate: Date) {
 	const millisecondsPerDay = 24 * 60 * 60 * 1000;
 	return Math.round((Date.now() - startDate.getMilliseconds()) / millisecondsPerDay);
 }
