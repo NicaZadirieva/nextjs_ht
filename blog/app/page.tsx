3269545152f44949d7bb1c733e0e7e4b888525dd
NicaZadirieva <no.demchenko@yandex.ru>
2025-07-08 15:19:17 +0300

    feat: add grid to blog items

--- blog/app/page.tsx
+++ blog/app/page.tsx
@@ -2,16 +2,20 @@ import { BlogItem } from '@/entities';
 
 export default function Home() {
 	return (
-		<div>
-			<BlogItem 
-				text="The CSS Grid Layout Module offers a grid-based layout system, with rows and columns, making it easier to design web pages without having to use floats and positioning." 
-				createdAt={new Date('2021-09-25')}
-				tag='Frontend'
-				title="Css Grid"
-				readTime="3 Min Read"
-				postLink="google.com"
-				mainPhotoLink='/Blog/blogPost.jpg'/>
-
+		<div className='blog-container'>
+			{
+				new Array(15).fill(0).map((val, i) => {
+					return (<BlogItem
+						key={i}
+						text="The CSS Grid Layout Module offers a grid-based layout system, with rows and columns, making it easier to design web pages without having to use floats and positioning."
+						createdAt={new Date('2021-09-25')}
+						tag='Frontend'
+						title="Css Grid"
+						readTime="3 Min Read"
+						postLink="google.com"
+						mainPhotoLink='/Blog/blogPost.jpg' />);
+				})
+			}
 			
 		</div>
 	);
