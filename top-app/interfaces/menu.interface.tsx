5e3779e9649c358f5bc397022bfb1e7a3395bffc
NicaZadirieva <no.demchenko@yandex.ru>
2025-07-10 09:04:26 +0300

    interfaces

---
+++ top-app/interfaces/menu.interface.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import { TopLevelCategory } from './page.interface';
+
+export interface PageItem {
+    alias: string;
+    title: string;
+    _id: string;
+    category: string;
+}
+
+export interface MenuItem {
+    _id: {
+        secondCategory: string;
+    };
+    isOpened?: boolean;
+    pages: PageItem[];
+}
+
+export interface FirstLevelMenuItem {
+    route: string;
+    name: string;
+    icon: React.JSX.Element;
+    id: TopLevelCategory;
+}
