0c782aaeb0565a25c4e1fe1ad597552efcce52c2
NicaZadirieva <no.demchenko@yandex.ru>
2025-07-10 09:49:16 +0300

    генерация статической страницы (SSG)

--- top-app/app/(site)/products/[alias]/page.tsx
+++ top-app/app/(site)/products/[alias]/page.tsx
@@ -1,3 +1,4 @@
+import { getMenu } from '@/api/menu';
 import { getPage } from '@/api/page';
 import { Metadata } from 'next';
 import { notFound } from 'next/navigation';
@@ -13,6 +14,10 @@ export async function generateMetadata(): Promise<Metadata> {
 	};
 }
 
+export async function generateStaticParams() {
+	const menu = await getMenu(0);
+	return menu.flatMap(item => item.pages.map(page => ({alias: page.alias})));
+}
 
 export default async function PageProducts({ params }: { params: {alias: string}}) {
 	const page = await getPage(params.alias);
