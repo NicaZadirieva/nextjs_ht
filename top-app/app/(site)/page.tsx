b6bffff0c55946ee3e0ab7312e1c25017d6a794b
NicaZadirieva <no.demchenko@yandex.ru>
2025-07-10 09:25:47 +0300

    add async page

--- top-app/app/(site)/page.tsx
+++ top-app/app/(site)/page.tsx
@@ -1,6 +1,6 @@
+import type { MenuItem } from '@/interfaces/menu.interface';
 import { Metadata } from 'next';
-import Arrow from './arrow.svg';
-import { Button, HTag, Paragraph, Rating, Tag } from './components';
+import { API } from '../api';
 
 // export const metadata: Metadata = {
 // 	title: 'Исправленные данные',
@@ -13,23 +13,25 @@ export async function generateMetadata() : Promise<Metadata>{
 		title: 'ComputedMetadata'
 	};
 }
-export default function Home() {
-	return (<>
-		<Button appearance='primary'>Кнопка</Button>
-		<Button appearance='ghost'>Кнопка</Button>
 
-		<HTag tag="h1">
-			Text
-		</HTag>
-		<span><Arrow /></span>
-		<Paragraph size='m'>Студенты освоят не только hard skills, необходимые для работы веб-дизайнером, но и soft skills — навыки, которые позволят эффективно взаимодействовать в команде с менеджерами, разработчиками и маркетологами. Выпускники факультета могут успешно конкурировать с веб-дизайнерами уровня middle.</Paragraph>
-		<Paragraph size='s'>Напишу сразу в двух курсах, так как проходил оба. Java будет многим непросвещённым сложновата в изучении, но здесь перевес из-за лидирующего положения языка как самого популярного в программировании. Выбор мой пал на эту профессию еще и потому, что Java-разработчики получают самую большую зарплату. Хотя Python начинает догонять Java по многим моментам, но вот в крупном екоме разработке Джава все-таки остается главенствующей сейчас. Скажу так – полнота программы и интенсивность присуща обоим курсам GeekBrains. Хочу отметить, что с первого дня занятий вы приступаете к практике и получаете опыт коммерческой разработки уже в свое резюме. Скажу вам как прошедший это – реально помогло в трудоустройстве!</Paragraph>
-		<Tag size='s' color='ghost'>Мал</Tag>
-		<Tag size='m' color='green'>Мал</Tag>
-		<Tag size='m' color='red'>Red</Tag>
-		<Tag size='s' color='primary'>primary</Tag>
-		<Paragraph size='l'>Выше указаны программы Adobe InDesign, Adobe Illustrator, Corel Draw и ими можно успешно пользоваться дома или в дороге. Современные ноутбуки хорошо справляются с нагрузкой, так зачем загонять специалиста в душный офис. В этой профессии важным считается вдохновение, поэтому дизайнеры ищут его в разных местах.</Paragraph>
-		<Rating rating={4} isEditable={true}/>
-	</>
+
+async function getMenu(firstCategory: number): Promise<MenuItem[]> {
+	const res = await fetch(API.topPage.find, {
+		method: 'POST',
+		body: JSON.stringify({
+			firstCategory
+		}),
+		headers: { 'Content-Type': 'application/json' }
+	});
+	return res.json();
+}
+export default async function Home() {
+	const menu = await getMenu(1);
+	return (<main>
+		Главная
+		<div>
+			<div>{JSON.stringify(menu)}</div>
+		</div>
+	</main>
 	);
 }
