038da5e83e7b78afa91ff2f033e4a436fecf4c6b
NicaZadirieva <no.demchenko@yandex.ru>
2025-08-04 10:50:19 +0300

    refactor: сделала провайдер. разделение логики

--- blog/app/shared/Header/index.tsx
+++ blog/app/shared/Header/index.tsx
@@ -1,23 +1,22 @@
 'use client';
-import { BaseContext } from '@/app/context';
+import { BaseContextProvider } from '@/app/context';
 import cn from 'classnames';
 import { Open_Sans } from 'next/font/google';
-import { useContext } from 'react';
-import GithubIcon from './icon/icon_github.svg';
 import styles from './index.module.css';
+import { GithubUrl } from './ui/GithubUrl';
 
 
 
 const openSans = Open_Sans({ subsets: ['latin'] });
 
 export const Header = () => {
-	const baseContext = useContext(BaseContext);
 	return (
-		<header className={styles.header}>
-			<h1 className={cn(styles.h1, openSans.className)}>.my_blog</h1>
-			<a title="Ссылка на гит" href={baseContext.GITHUB_URL}>
-				<GithubIcon width={30} height={30} />
-			</a>
-		</header>
+		<BaseContextProvider>
+			<header className={styles.header}>
+				<h1 className={cn(styles.h1, openSans.className)}>.my_blog</h1>
+				<GithubUrl/>
+			</header>
+		</BaseContextProvider>
+		
 	);
 };
---
+++ blog/app/shared/Header/ui/GithubUrl/index.tsx
@@ -0,0 +1,12 @@
+import { BaseContext } from '@/app/context';
+import { useContext } from 'react';
+import GithubIcon from './icon/icon_github.svg';
+
+export const GithubUrl = () => {
+	const baseContext = useContext(BaseContext);
+	return (
+		<a title="Ссылка на гит" href={baseContext.GITHUB_URL}>
+			<GithubIcon width={30} height={30} />
+		</a>
+	);
+};
