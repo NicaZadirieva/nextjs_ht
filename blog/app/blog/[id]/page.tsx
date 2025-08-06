785dd898ca36f27070bf71a7b300742b3e2b47b8
NicaZadirieva <no.demchenko@yandex.ru>
2025-08-06 22:46:00 +0300

    chore: add comment

--- blog/app/blog/[id]/page.tsx
+++ blog/app/blog/[id]/page.tsx
@@ -19,6 +19,7 @@ export default async function BlogPost({ params }: Params) {
 	if (!res) {
 		return notFound();
 	}
+	// временная заглушка для контента страницы поста
 	return (
 		<>{res.content}</>
 	);
